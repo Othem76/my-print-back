dc1912b1a62fa104a9d90d9c7383b18eae240774
Gabriel Gressier <gabfreestyle@gmail.com>
2024-10-23 22:05:07 +0200

    fix: routes updated to work in the docker

--- start/routes.ts
+++ start/routes.ts
@@ -1,8 +1,14 @@
 import router from "@adonisjs/core/services/router";
+import GetAllUsersController from "#controllers/user/getAllUsersController";
+import GetUserByIdController from "#controllers/user/getUserByIdController";
+import GetUserByEmailController from "#controllers/user/getUserByEmailController";
+import CreateUserController from "#controllers/user/createUserController";
+import UpdateUserController from "#controllers/user/updateUserController";
+import DeleteUserController from "#controllers/user/deleteUserController";
 
-router.get("/getAllUsers", "#controllers/user/GetAllUsersController");
-router.get("/getUserById/:id", "#controllers/user/GetUserByIdController");
-router.get("/getUserByEmail", "#controllers/user/GetUserByEmailController");
-router.post("/createUser", "#controllers/user/CreateUserController");
-router.patch("/updateUser", "#controllers/user/UpdateUserController");
-router.delete("/deleteUser/:id", "#controllers/user/DeleteUserController");
+router.get("/getAllUsers", [GetAllUsersController]);
+router.get("/getUserById/:id", [GetUserByIdController]);
+router.get("/getUserByEmail", [GetUserByEmailController]);
+router.post("/createUser", [CreateUserController]);
+router.patch("/updateUser", [UpdateUserController]);
+router.delete("/deleteUser/:id", [DeleteUserController]);
