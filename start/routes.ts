9ece66da14551759590fcf129ab5d34352b5bd23
tmouisse <tmouisse@access-it.fr>
2024-10-29 15:37:53 +0100

    standardize users routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,9 @@
 import UserController from "#controllers/user/userController";
 import router from "@adonisjs/core/services/router";
 
-router.get('getAllUsers', [UserController, 'getAll'])
-router.get('getUserById/:id', [UserController, 'getUserById'])
-router.get('getUserByEmail', [UserController, 'getUserByEmail'])
-router.post('createUser', [UserController, 'create'])
-router.patch('updateUser', [UserController, 'update'])
-router.delete('deleteUser/:id', [UserController, 'delete'])
+router.get("users", [UserController, "getAll"]);
+router.get("users/:id", [UserController, "getUserById"]);
+router.get("users/by-email", [UserController, "getUserByEmail"]);
+router.post("users", [UserController, "create"]);
+router.put("users", [UserController, "update"]);
+router.delete("users/:id", [UserController, "delete"]);
