3ff5da4e3c16092ec4d224f61eba5f20390752f5
Zackaliok <zackaliok.contact@gmail.com>
2024-10-31 14:12:27 +0100

    Upload One or Many files into storage/stl

---
+++ app/controllers/stl/stlController.ts
@@ -0,0 +1,101 @@
+import { HttpContext } from '@adonisjs/core/http'
+import app from '@adonisjs/core/services/app'
+import { cuid } from '@adonisjs/core/helpers'
+
+export default class StlController {
+  
+  async uploadOne({ request, response }: HttpContext) {
+    const file = request.file('stlFile')
+
+    if (!file) {
+      return response.status(400).send({
+        errors: [
+          {
+            message: 'No file uploaded'
+          }
+        ]
+      })
+    }
+    
+    // Type verification
+    if (file.extname && file.extname !== 'stl') {
+      return response.status(400).send({
+        errors: [
+          {
+            message: 'The file must be a STL file'
+          }
+        ]
+      })
+    }
+
+    // Alternative type verification
+    const extension = file.clientName.split('.').pop()?.toLocaleLowerCase()
+    if (extension !== 'stl') {
+      return response.status(400).send({
+        errors: [
+          {
+            message: 'The file must be a STL file'
+          }
+        ]
+      })
+    }
+
+    await file.move(app.makePath('storage/uploads'), {
+      name: `${cuid()}.stl`
+    })
+
+    return response.ok({ message: 'Fichier uploadé avec succès.' })
+  }
+
+  async uploadMany({ request, response }: HttpContext) {
+    const files = request.files('stlFiles', {
+      extnames: ['stl'],
+    })
+
+    if (files.length === 0) {
+      return response.status(400).send({
+        errors: [
+          {
+            message: 'No file uploaded'
+          }
+        ]
+      })
+    }
+
+    for (let file of files) {
+      // Type verification
+      if (file.extname && file.extname !== 'stl') {
+        return response.status(400).send({
+          errors: [
+            {
+              message: `The file ${file.clientName} must be a STL file`
+            }
+          ]
+        })
+      }
+
+      // Alternative type verification
+      const extension = file.clientName.split('.').pop()?.toLocaleLowerCase()
+      if (extension !== 'stl') {
+        return response.status(400).send({
+          errors: [
+            {
+              message: `The file ${file.clientName} must be a STL file`
+            }
+          ]
+        })
+      }
+    }
+
+    // Files storage
+    for (let file of files) {
+      file.move(app.makePath('storage/uploads'), {
+        name: `${cuid()}.stl`
+      })
+    }
+
+    return response.ok({ message: 'Tout les fichiers uploadés avec succès.' })
+  }
+
+
+}
