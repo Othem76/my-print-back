064064958c0e09384c2993f1742ec63e43a67524
Gabriel Gressier <gabfreestyle@gmail.com>
2025-01-09 10:31:26 +0100

    feat: swagger for printer done

--- app/controllers/printer/printerController.ts
+++ app/controllers/printer/printerController.ts
@@ -6,6 +6,12 @@ import { HttpContext } from "@adonisjs/core/http";
 export default class PrinterController {
   constructor(private readonly printerService: PrinterService) {}
 
+  /**
+   * @getAllPrinters
+   * @description Get all printers
+   * @responseBody 200 - [{"id":6,"name":"CREALITY CR10-S5","plateSize":"500 x 500 x 500","cleaningCost":2,"impressingCost":0.03822},{"id":5,"name":"ULTIMAKER S3","plateSize":"230 x 190 x 200","cleaningCost":2,"impressingCost":0.03822},{"id":4,"name":"CREALITY CR10-S5","plateSize":"500 x 500 x 500","cleaningCost":2,"impressingCost":0.03822},{"id":3,"name":"ULTIMAKER S3","plateSize":"230 x 190 x 200","cleaningCost":2,"impressingCost":0.03822},{"id":2,"name":"CREALITY CR10-S5","plateSize":"500 x 500 x 500","cleaningCost":2,"impressingCost":0.03822},{"id":1,"name":"ULTIMAKER S3","plateSize":"230 x 190 x 200","cleaningCost":2,"impressingCost":0.03822}]
+   * @responseBody 404 - { "errors": [ { "message": "Printers not found" } ] }
+   */
   async getAllPrinters({ response }): Promise<HttpContext> {
     try {
       const printers = await this.printerService.getAllPrinters();
@@ -15,6 +21,13 @@ export default class PrinterController {
     }
   }
 
+  /**
+   * @getPrinterById
+   * @description Get a printer by ID
+   * @responseBody 200 - {"id":1,"name":"ULTIMAKER S3","plateSize":"230 x 190 x 200","cleaningCost":2,"impressingCost":0.03822}
+   * @responseBody 400 - { "errors": [ { "message": "Printer ID is required" } ] }
+   * @responseBody 404 - { "errors": [ { "message": "Printer not found" } ] }
+   */
   async getPrinterById({ params, response }) {
     const printerId = params.id;
     if (!printerId) {
@@ -29,6 +42,13 @@ export default class PrinterController {
     }
   }
 
+  /**
+   * @deletePrinter
+   * @description Delete a printer by ID
+   * @responseBody 200 - {"id":1,"name":"ULTIMAKER S3","plateSize":"230 x 190 x 200","cleaningCost":2,"impressingCost":0.03822}
+   * @responseBody 400 - { "errors": [ { "message": "Printer ID is required" } ] }
+   * @responseBody 404 - { "errors": [ { "message": "Printer not found" } ] }
+   */
   async deletePrinter({ params, response }) {
     const printerId = params.id;
 
