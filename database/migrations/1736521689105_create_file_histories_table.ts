b07014441bb3fc54c04a519ca46a094d06caf6a0
Zackaliok <zackaliok.contact@gmail.com>
2025-01-10 16:16:10 +0100

    file history migration

---
+++ database/migrations/1736521689105_create_file_histories_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'file_histories'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('file_original_name').notNullable()
+      table.string('file_server_name').notNullable().unique()
+      table.integer('user_id').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
