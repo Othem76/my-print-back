5b97ea8f9cd3cdbe9b3d6cf8bcdf24bffbe940f3
Zackaliok <zackaliok.contact@gmail.com>
2025-01-10 16:16:10 +0100

    file history migration

---
+++ app/models/fileHistory.ts
@@ -0,0 +1,22 @@
+import { BaseModel, column } from "@adonisjs/lucid/orm";
+import { DateTime } from "luxon";
+
+export default class FileHistory extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public fileOriginalName: string
+
+  @column()
+  public fileServerName: string
+
+  @column()
+  public userId: number
+
+  @column.dateTime({ autoCreate: true })
+  public uploadedAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime | null
+}
