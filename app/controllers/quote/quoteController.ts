6ec9bbc156314848d991b82b89f737766591d7f5
Zackaliok <57079892+Zackaliok@users.noreply.github.com>
2025-01-31 15:15:49 +0100

    Feat: kan-44
    * file history migration
    
    * [FILE HISTORY] :
    
    - FileHistoryService
    - FileHistoryRespository and Interface
    - FileHistory model (with id, fileOriginalName, fileServerName, userId, status, uploadedAt and updatedAt)
    - Payloads and status enum
    - Update Migration to add status column
    
    * [FILE HISTORY]: Add status to payload
    
    * [FILE HISTORY]: Use of fileHistoryService in stl upload controller
    
    * [FILE HISTORY]: Fix column name in migration + add auth to upload routes
    
    * [FILE HISTORY]: Fix updateHistory. Protecting primary key from force update
    
    * commit nul
    
    * [DELETE FILE]: delete after sending the quote:
    
    - Adding fileServerName to the quote request for file identification.
    - File history update (log reasons).
    - Returning file history when we upload a new stl.

--- app/controllers/quote/quoteController.ts
+++ app/controllers/quote/quoteController.ts
@@ -1,9 +1,20 @@
 import { HttpContext } from "@adonisjs/core/http";
 import QuotePdfPayload from "#interfaces/quote/quotePdfPayload.js";
+import { FILE_UPLOAD_DIRECTORY } from "../../utils/consts.js";
+import fs from 'fs'
+import FileHistoryService from "#services/fileHistoryService";
+import QuoteService from "#services/quoteService";
+import { inject } from "@adonisjs/core";
+import { HistoryStatus } from "#interfaces/fileHistory/historyStatus";
 
+@inject()
 export default class QuoteController {
+
+  constructor(private fileHistoryService: FileHistoryService, private quoteService: QuoteService) {}
+
   async generatePdf({ request, response }: HttpContext) {
     const payload: QuotePdfPayload = request.only([
+      "fileServerName",
       "title",
       "description",
       "date",
@@ -17,15 +28,38 @@ export default class QuoteController {
       "totalCost",
     ]);
 
-    const {default: QuoteService} = await import("#services/quoteService")
 
     try {
-      const pdfBuffer = await QuoteService.generatePdf(payload)
+      const pdfBuffer = await this.quoteService.generatePdf(payload)
 
       const filename = new Date().toISOString().slice(0, 10).replace(/-/g, '')
 
       response.header('Content-Type', 'application/pdf')
       response.header('Content-Disposition', `attachment; filename="devis-${filename}.pdf"`)
+
+      // Suppression du fichier stl temporaire
+      const filePath = `${FILE_UPLOAD_DIRECTORY}/${payload.fileServerName}`
+
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`Error deleting file ${payload.fileServerName}: ${err.message}`)
+        } else {
+          console.log(`Deleted file: ${payload.fileServerName}`)
+        }
+      })
+
+      // Mise a jour de l'historique du fichier
+      const history = await this.fileHistoryService.getByFileServerName(payload.fileServerName);
+
+      if (history) {
+        const updateFileHistoryPayload = {
+          id: history.id,
+          status: HistoryStatus.Finished,
+        }
+
+        this.fileHistoryService.updateHistory(updateFileHistoryPayload)
+      }
+
       return response.send(pdfBuffer)
     } catch (error) {
       return response.status(500).send({ error: 'Une erreur est survenue lors de la génération du PDF' })
